48fe1ff4ecdae83e89da5b06c321b65fdcf4b6f3
Eric <deleoneric97@gmail.com>
2021-04-23 12:48:16 -0400

    First Commit

---
+++ src/build/precache-manifest.03e705319c859fd981517928424157a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e49dcdde0a5f04bafc32",
+    "url": "/static/css/main.f49c1f3d.chunk.css"
+  },
+  {
+    "revision": "e49dcdde0a5f04bafc32",
+    "url": "/static/js/main.fa6d9dd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72546995997ab1773b6",
+    "url": "/static/js/2.f96445f7.chunk.js"
+  },
+  {
+    "revision": "fd03c163d4a3562e5f817318f14de998",
+    "url": "/index.html"
+  }
+];
